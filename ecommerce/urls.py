2822290c600d426250384dab1f19343ee5185fa3
emerson <emersoneletrotecnico2013@hotmail.com>
2024-11-26 14:42:14 -0300

    ecommerce
    criar os modelos de produtos e categorias
    obs: precisei comentar as urls de accounts e products para fazer a migração

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')), # URLs de contas
-    path('products/', include('products.urls')), # URLs de Produtod
+    #path('accounts/', include('accounts.urls')), # URLs de contas
+    #path('products/', include('products.urls')), # URLs de Produtod
 ]
