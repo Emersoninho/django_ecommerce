23e6226ccf0d00d81b6c8a7d8913bce00ccd2485
emerson <emersoneletrotecnico2013@hotmail.com>
2024-11-26 15:30:41 -0300

    ecommerce
    criando a Views, URLs e Templates
    OBB: voltei com a URL de product comentada

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('accounts/', include('accounts.urls')), # URLs de contas
-    #path('products/', include('products.urls')), # URLs de Produtod
+    path('products/', include('products.urls')), # URLs de Produtod
 ]
