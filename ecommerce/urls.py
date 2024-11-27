625262016c634453dc7d729d590ffbd26a23406c
emerson <emersoneletrotecnico2013@hotmail.com>
2024-11-27 09:38:18 -0300

    ecommerce
    configurando o checkout, criando os modelos de pedido e endereço, migração dos modelos, configurando o admin, criando Views para o checkout, criando o formulario de endereço, URLs do pedido e incluindo a URL no arquivo principal.
    OBS: esqueci se separar as etapas por isso ficou tão crende

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('accounts/', include('accounts.urls')), # URLs de contas
     path('products/', include('products.urls')), # URLs de Produtod
-    path('cart/', include('cart.urls')), # inclui as URLS do carrinho
+    path('cart/', include('cart.urls')), # inclui as URLS do 
+    path('orders/', include('orders.urls')) # inclui as URLs de ordem
 ]
